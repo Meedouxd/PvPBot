126c71c2df85d62d01d49033cb497130c52a08b9
Meedouxd <54485614+Meedouxd@users.noreply.github.com>
2021-10-11 21:11:18 -0400

    Update PvPBotMod.java

--- src/main/java/com/m33dz/pvpbot/PvPBotMod.java
+++ src/main/java/com/m33dz/pvpbot/PvPBotMod.java
@@ -5,10 +5,11 @@ import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EntityPigZombie;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.MathHelper;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -16,6 +17,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @Mod(modid = PvPBotMod.MODID, version = PvPBotMod.VERSION)
@@ -26,12 +28,14 @@ public class PvPBotMod
     public static final String VERSION = "1.0";
     PvPHandler pvpHandler = new PvPHandler();
     Target target = new Target();
+    Keybinds keybinds = new Keybinds();
 
     @EventHandler
     public void PreInit(FMLPreInitializationEvent event)
     {
         FMLCommonHandler.instance().bus().register(this);
         MinecraftForge.EVENT_BUS.register(this);
+        Keybinds.register();
     }
     @EventHandler
     public void init(FMLInitializationEvent event)
@@ -48,16 +52,6 @@ public class PvPBotMod
     @SubscribeEvent
     public void onChat(ClientChatReceivedEvent event){
         String message = event.message.getUnformattedText();
-        if(message.equals("<" + mc.thePlayer.getName() + ">" + " !pvp")){
-            pvpHandler.toggle();
-            if(pvpHandler.isOn){
-                mc.thePlayer.sendChatMessage("PvpBot: activated");
-            }
-            else {
-                mc.thePlayer.sendChatMessage("PvpBot: deactivated");
-            }
-
-        }
     }
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent event){
@@ -65,9 +59,8 @@ public class PvPBotMod
             if(pvpHandler.isOn){
                 mc.thePlayer.setSprinting(true);
                 checkTarget();
+                lookAtEntity();
                 checkHealth();
-                //mc.thePlayer.rotationYaw = 0;
-                //mc.thePlayer.rotationPitch = 0;
             }
         }
     }
@@ -79,7 +72,7 @@ public class PvPBotMod
         if(!target.hasATarget){
             for (Entity e : mc.theWorld.loadedEntityList) {
                 if (e instanceof EntityOtherPlayerMP) {
-                    if (mc.thePlayer.getDistanceSqToEntity(e) <= 3.8f && e.isEntityAlive()) {
+                    if (mc.thePlayer.getDistanceSqToEntity(e) <= 26.0f && e.isEntityAlive()) {
                         proccessHit(e);
                     }
                 }
@@ -95,6 +88,48 @@ public class PvPBotMod
         if(mc.thePlayer.getHealth() >= 5.0){
             KeyBinding.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), false);
             mc.thePlayer.inventory.currentItem = 0;
+        }
+    }
+    public void checkPearlThrow(){
+        if(mc.thePlayer.getHealth() >= 10.0){
+            for (Entity e : mc.theWorld.loadedEntityList) {
+                if (e instanceof EntityOtherPlayerMP) {
+                    if (mc.thePlayer.getDistanceSqToEntity(e) >= 26.0f && e.isEntityAlive()) {
+                        mc.thePlayer.inventory.currentItem = 2;
+                        KeyBinding.setKeyBindState(mc.gameSettings.keyBindUseItem.getKeyCode(), true);
+                    }
+                }
+            }
+        }
+    }
+    public void lookAtEntity(){
+        for (Entity e : mc.theWorld.loadedEntityList) {
+            if (e instanceof EntityOtherPlayerMP) {
+                if (mc.thePlayer.getDistanceSqToEntity(e) <= 1000.0f && e.isEntityAlive()) {
+                    double deltaX = e.posX - mc.thePlayer.posX;
+                    double deltaY = e.posY - mc.thePlayer.posY;
+                    double deltaZ = e.posZ - mc.thePlayer.posZ;
+                    double dist = MathHelper.sqrt_double(deltaX * deltaX + deltaZ * deltaZ);
+                    mc.thePlayer.rotationYaw = (float) (Math.atan2(deltaZ, deltaX) * 180.0D / Math.PI) - 90.0F;
+                    mc.thePlayer.rotationPitch = (float) -(Math.atan2(deltaY, dist) * 180.0D / Math.PI);
+                }
+            }
+        }
+    }
+    @SubscribeEvent
+    public void onKeyInput(InputEvent.KeyInputEvent event)
+    {
+        if (Keybinds.pvp.isPressed())
+        {
+            pvpHandler.toggle();
+            if(pvpHandler.isOn){
+                mc.thePlayer.addChatMessage(new ChatComponentText("[PvPBot] activated"));
+                KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), true);
+            }
+            else {
+                mc.thePlayer.addChatMessage(new ChatComponentText("[PvPBot] deactivated"));
+                KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKeyCode(), false);
+            }
         }
     }
 }
