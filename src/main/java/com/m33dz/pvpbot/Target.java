63f9efe2c99d8befee00db0d64e040054215fa71
Meedouxd <54485614+Meedouxd@users.noreply.github.com>
2021-12-18 10:34:19 -0500

    Update Target.java

--- src/main/java/com/m33dz/pvpbot/Target.java
+++ src/main/java/com/m33dz/pvpbot/Target.java
@@ -5,16 +5,27 @@ import net.minecraft.entity.Entity;
 public class Target {
     public Entity target;
     public boolean hasATarget = false;
+    public int targetID = 0;
     public void setTarget(Entity entity){
             entity = target;
             hasATarget = true;
     }
     public Entity getTarget(){
-        return target;
+        if(target != null){
+            return target;
+        }else{
+            return null;
+        }
     }
     public void checkTarget(){
         if(target == null){
             hasATarget = false;
         }
+    }
+    public void setTargetID(int id){
+        targetID = id;
+    }
+    public int getTargetID(){
+        return targetID;
     }
 }
